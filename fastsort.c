f0cba0fb7a76e89426b23489705198a2bdac4b91
guneetsinghmehta <mehta.guneet@gmail.com>
2016-01-27 13:49:36 -0600

    getting number of lines in a file using numLines function

--- fastsort.c
+++ fastsort.c
@@ -1,21 +1,38 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+int numLines(char*);
 int main(int argc ,char *argv[])
 {
 	//Argument Check -starts
-	if(argv[1][0]=='-'&&argc==3){}	
+	if(argv[1][0]=='-'&&argc==3){printf("Correct\n");}	
 	else{fprintf(stderr,"Error: Bad command line parameters");exit(1);}
 	//Argument Check -ends
 	
 	char filename[300];	strcpy(filename,argv[2]);
 	FILE *f1;	f1=fopen(filename,"r");
-	if(f1<=0){fprintf("Error: Cannot open file %s",filename);exit(1);}
-	printf("%d",f1); 										
+	if(fileno(f1)<=0){fprintf(stderr,"Error: Cannot open file %s",filename);exit(1);}
+	printf("%d\n",fileno(f1)); 										
 	char temp[128];
 	while(fgets(temp,128,f1)!=NULL)
 	{
-		printf("%s\n",temp);
+		printf("%s",temp);
 	}
-	
+	printf("num of lines in file =%d word to be used =%d",numLines(filename),abs(atoi(argv[1])));
 	return 0;
+} 	
+
+int numLines(char *filename)
+{
+	FILE *f1;char c;
+	int numOfLines=0;
+	f1=fopen(filename,"r");
+	do
+	{
+		c=fgetc(f1);
+		if(c=='\n'){numOfLines++;}
+	}while(c!=EOF);
+	//if(c!='\n'&&numOfLines>0){numOfLines++;}//because last line does not have a new line character . Skip this because every line is separated by a new line
+	fclose(f1);
+	return numOfLines;
 }
