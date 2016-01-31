b2280f4b65dbf24608c21ad90abc5cc26a75d2da
Guneet Mehta <guneet@macaroni-09.cs.wisc.edu>
2016-01-30 20:29:13 -0600

    input arguments and error handling done file opening and testing done

--- fastsort2.c
+++ fastsort2.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 # define MAX 128
 int numLinesfn(char*);
 void getWord(char* ,char*,int);
@@ -9,29 +13,37 @@ int main(int argc ,char *argv[])
 	//tasks remaining - malloc
 	
 	//Argument Check -starts
-	char filename[300], *c;
-	int wordIndex;
-	int i,j,k,temp;
+	char filename[300],temp[MAX];
+	int wordIndex,numLines;
+	int i;
 
 	if(argc==2){strcpy(filename,argv[1]);wordIndex=1;}
-	else if(argc==3&&argv[1][0]=='-'&&strtol(&argv[1][1],c,0)>0)
+	else if(argc==3&&argv[1][0]=='-')
 	{
 		strcpy(filename,argv[2]);
-		wordIndex=strtol(&argv[1][1],c,0);
+		wordIndex=abs(atoi(argv[1]));
 		printf("normal 3");
 	}
-	else if(argc==3&&argv[1][0]=='-'&&strtol(&argv[1][1],c,0)==0)
-	{
-		strcpy(filename,argv[2]);wordIndex=strtol(&argv[1][1],c,0);
-		wordIndex=1;
-		printf("abnormal 3");
-	}
+
 	else 
 	{
 		fprintf(stderr,"Error: Bad command line parameters");exit(1);
 	} 
 	printf("wordIndex=%d and filename=%s",wordIndex,filename);
+	//Argument Chek -ends
+	
+	//opening file -starts
 	FILE *f1;
+	f1=fopen(filename,"r");
+	if(fileno(f1)<=0){fprintf(stderr,"Error: Cannot open file %s",filename);exit(1);}
+	printf("\n");	i=0;
+	while(fgets(temp,MAX,f1)!=NULL)	{	printf("%s",temp);	i++;	}//printing file contents
+	numLines=i;
+	//opening file -ends
+
+
+	
+	
 	
 	return 0;
 } 	
@@ -68,3 +80,15 @@ int numLinesfn(char *filename)
 	fclose(f1);
 	return numOfLines;
 }
+
+/*
+	Routine to check the file size
+	int file=0;
+        if((file=open(filename,O_RDONLY)) < -1)
+            return 1;
+	struct stat fileStat;
+	if(fstat(file,&fileStat) < 0)    
+		return 1;
+	printf("\nfile size=%d ",fileStat.st_size);
+	
+*/
