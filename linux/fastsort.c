d8a64458d998e390cb998d564a95ea1d2f6bf2bf
Anmol Mohanty <anmolnehru@gmail.com>
2016-02-02 18:54:12 -0600

    fastsort before TA help

--- linux/fastsort.c
+++ linux/fastsort.c
@@ -7,39 +7,55 @@
 #include<stdlib.h>
 #include<string.h>
 
-void main(int argc, char* argv)
+
+//structure for key value pair
+typedef struct Hashmap {
+    int key;
+    char *value;
+} Hashmap;
+
+//function prototype
+static int comp_strings(const void *p1, const void *p2);
+
+
+void main(int argc, char* argv[])
 {
 
+//printf("arg counts=%d",argc);
 //arguments check logic
-
 //invalid number of arguments //throw error
-if(argc<0 && argc >2)
+if(argc<2 || argc >3)
 {
-//error message
-fprintf(stderr, “Error: Bad command line parameters”);
+//error message tp stderror - mentioned on webpage
+fprintf(stderr, "Error: Bad command line parameters\n");
 //clean exit
 exit(1);
 }
 
 else
 {
-
-
-if(argc==1)
-compare_index=0;
-else
+//stores the index of the word that will be used for comparision
+int compare_index=0;//the default
+if(argc!=1)
 //take care of the hyphen
-compare_index=argv[1];
+compare_index=abs(atoi(argv[1]))-1;
+//printf("compare_index %d",compare_index);
+//exit(0);
 
 FILE *fp;
 
 //file opens
-fp = fopen("argv[2]","r");
-
+fp = fopen(argv[2],"r");
+if(fp==NULL)
+{
+fprintf(stderr, "Error: Cannot open file %s\n",argv[2]);
+exit(1);
+}
 //get number of lines in file
 
 int count_lines=0;
 //iterates throug chars and counts instances of \n till end of file
+int ch=0;
 while(!feof(fp))
 {
   ch = fgetc(fp);
@@ -55,21 +71,46 @@ while(!feof(fp))
 
 //capturing the lines 1 by 1
 //max 128 chars in a line
-char lines[count_lines][128];
+//char *lines[128] = (char *)malloc(count_lines);
+
+//capturing the individual lines
+char **lines = (char**) calloc(count_lines, sizeof(char*));
+char **lines_cpy = (char**) calloc(count_lines, sizeof(char*)); //copy to tokenize
+if(lines==NULL || lines_cpy==NULL)
+{
+fprintf(stderr,"malloc failed\n");
+exit(1);
+}
+
+int i=0;
+for ( i = 0; i < count_lines; i++ )
+{
+    lines[i] = (char*) calloc(128, sizeof(char)); //max length = 128
+    lines_cpy[i] = (char*) calloc(128, sizeof(char)); //max length = 128
+//	printf("allocing memory for line #%d\n",i);
+if(lines[i]==NULL || lines_cpy[i]==NULL)
+	
+	{
+	fprintf(stderr,"malloc failed\n");
+	exit(1);
+	}
+}
 
 int iter=0;
-
+//BEGIN FGET
 while(iter<count_lines)
 {	
 	//gets lines
-	fgets(lines[iter],128,fp);
+	if(fgets(lines[iter],20,fp)==NULL){
+	 fprintf(stderr,"fgets failed");
+	 exit(1);
+	}
+	//printf("Hello %s\n",*lines[iter]);
 	//check for last char
-//	if(lines[iter][127]!='\n'
-//	//last character
-	if(strlen(lines[iter]<128);	//ok, do nothing
+	if(strlen(lines[iter])<128) printf("%s\n",lines[iter]);	//ok, do nothing
 	else if(lines[iter][127]!='\n')//whoops
 	{
-		fprintf(stderr, “Line too long”);
+		fprintf(stderr, "Line too long\n");
 		exit(1);
 	}
 	iter++;
@@ -78,13 +119,65 @@ while(iter<count_lines)
 //from copy strtok the word into a the array
 //then call qsort(on 2 D array) and return right indices
 
-char *temp;
-while(iter<=compare_index)
+//structure to sort
+
+
+//char lines_cpy[count_lines][128]; //copy of lines to tokenize on
+
+
+//char lines_sort[count_lines][128];
+for(i=0;i<count_lines;i++)
 {
+   /* get the first token, delim is a space */
+   char *token = strtok(lines_cpy[i], " ");
+   int iter=0;
+   char *last=""; //empty string default value, if nostring or space strings
+   /* walk through other tokens */
+   while( token != NULL && iter <compare_index ) 
+   {	
+      last=token;
+      printf( " %s\n", token );
+      token = strtok(NULL, " ");
+	iter++;
+   }
 
-pch=str
+if(token==NULL)//last word reached
+lines_cpy[i]=last;
+else
+lines_cpy[i]=token;
+}
 
 
 
+Hashmap tosort[count_lines];
+
+for(i=0;i<count_lines;i++)
+{
+tosort[i].key=i;
+tosort[i].value=lines_cpy[i];
+}
 
 
+//begin sorting
+
+qsort(tosort, count_lines, sizeof(Hashmap),comp_strings);
+
+//jettison the output
+for(i=0;i<count_lines;i++)
+//ask if this is ok
+printf("%s\n",lines[(tosort[i].key)]);
+
+//close the file
+fclose(fp);
+}
+
+
+}
+
+static int comp_strings(const void *p1, const void *p2)
+{
+  //int aa,bb;
+  //aa=(*(rec_t *)p1).key;
+  //bb=(*(rec_t *)p2).key;
+  return (strcmp((*(Hashmap *)p1).value,(*(Hashmap *)p2).value));
+}
