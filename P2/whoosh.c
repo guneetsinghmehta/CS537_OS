7396d2acd9484fd9db6502547305bf5a4179d1e0
Guneet Mehta <guneet@macaroni-08.cs.wisc.edu>
2016-02-10 17:44:36 -0600

    Running whoosh

--- P2/whoosh.c
+++ P2/whoosh.c
@@ -5,31 +5,80 @@
 #include <sys/wait.h>
 #include<string.h>
 #include <fcntl.h>
-void errorFn()
-{
+
+
+void callErrorFn()
+{	
 	char error_message[30]="An error has occured\n";
 	write(STDERR_FILENO,error_message,strlen(error_message));
 }
 
+void parse(char *input , char **command)
+{
+	const char s[2]=" ";
+	char *inputCopy=strdup(input);
+	char *token;
+	int i=0;
+	token=strtok(inputCopy,s);
+	while(token!=NULL)
+	{
+		//printf("%s ",token);
+		command[i]=strdup(token);
+		i++;
+		token=strtok(NULL,s);
+	}
+}
+
+void searchCommand(char *input)
+{
+	//searches for commands in the specified paths
+}
+/*
+	3 split into tokens
+	4 depending on tokens - 
+	a. if in built then call the function
+	b. if cd /exit/pwd /path then call branches in code - make functions in fact
+	
+	1. parse into chars
+	2. search command
+	3. execute command
+*/
+
 int main(int argc ,char *argv[])
 {	
-	int rc=fork();	
-	if(rc==0)
-	{
-		char *myargv[4];
-		myargv[0]=strdup("/bin/ls");
-		myargv[1]=strdup("-l");
-		myargv[2]=strdup("-a");
-		myargv[3]=NULL;
-		execv(myargv[0],myargv);
-		printf("failed\n");
-		exit(1);
-	}
-	else if(rc>0)
-	{
-		int cpid=(int)wait(NULL);
-		printf("parent : cpid was %d\n",cpid);
-	}
-	else{printf("failure");}	
+	char input[128];
+	char **command;	command=malloc(128);// will contain ls -la /tmp etc etc ...
+
+	printf("whoosh> ");
+	fgets(input,128,stdin);
+	parse(input,command);
+		
 	return 0;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
