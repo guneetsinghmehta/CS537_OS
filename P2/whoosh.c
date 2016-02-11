2354f3eec9eea09417d99d22a94a6fb507aeeb97
Guneet Mehta <guneet@macaroni-08.cs.wisc.edu>
2016-02-10 19:08:39 -0600

    working version with ls

--- P2/whoosh.c
+++ P2/whoosh.c
@@ -5,16 +5,24 @@
 #include <sys/wait.h>
 #include<string.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #define MAXLINELENGTH 128
 
 void callErrorFn()
 {	
+	//all functions call this function to display error
 	char error_message[30]="An error has occured\n";
 	write(STDERR_FILENO,error_message,strlen(error_message));
+	exit(1);
 }
 
 void parse(char *input , char **command)
 {
+	/*
+		Returns command parsed as an array of strings ending at NULL
+	*/
 	const char s[2]=" ";
 	char *inputCopy=strdup(input);
 	char *token;
@@ -27,28 +35,82 @@ void parse(char *input , char **command)
 		i++;
 		token=strtok(NULL,s);
 	}
+	const char s2[2]="\n";
+	token=strtok(command[i-1],s2);
+	command[i-1]=strdup(token);
+	command[i]=NULL;
+	i=0;
 }
 
-void searchCommand(char *command,char **path)
+void searchCommand(char **command,char **path)
 {
 	//searches for commands in the specified paths
+	/*
+	takes in a command array. Loops over all paths specified and looks for the command
+	returns normally if command is present. 
+	If command is not present in any line then it displays an Error
+	*/
 	int i=0;
-	//FILE *f1;
 	char *temp=malloc(2*MAXLINELENGTH);
+	char *temp2=malloc(2*MAXLINELENGTH);
 	while(path[i]!=NULL)
 	{
-		temp=strcat(path[i],"/");
-		temp=strcat(temp,&command[0]);
-		printf(temp);
+		temp2=strdup(path[i]);
+		temp=strcat(temp2,"/");
+		temp=strcat(temp,&command[0][0]);
+		struct stat *buf=malloc(sizeof(struct stat));
+
+		if(stat(temp,buf)==0)
+		{
+			return;//success
+		}
 		i++;
 	}
+	callErrorFn();
+}
+
+void executeCommand(char **command, char **path)
+{
+	/*
+		Searches for command present in one of the search folders
+		On finding the command in a folder makes a child process and executes the command with 			parameters
+	*/
+ 	int i=0;
+	char *temp=malloc(2*MAXLINELENGTH);
+	char *temp2=malloc(2*MAXLINELENGTH);
+	while(path[i]!=NULL)
+	{
+		temp2=strdup(path[i]);
+		temp=strcat(temp2,"/");
+		temp=strcat(temp,&command[0][0]);
+		struct stat *buf=malloc(sizeof(struct stat));
+		if(stat(temp,buf)==0)
+		{
+			int rc=fork();
+			if(rc==0)
+			{
+				execv(temp,command);
+				callErrorFn();				
+			}
+			else if(rc>0)
+			{
+				int cpid=(int)wait(NULL);
+			}
+			else
+			{
+					callErrorFn();				
+			}
+			return;//success
+		}
+		i++;
+	}
+
 }
 /*
 	4 depending on tokens - 
 	a. if in built then call the function
 	b. if cd /exit/pwd /path then call branches in code - make functions in fact
 	
-	2. search command
 	3. execute command
 */
 
@@ -64,7 +126,8 @@ int main(int argc ,char *argv[])
 	printf("whoosh> ");
 	fgets(input,MAXLINELENGTH,stdin);
 	parse(input,command);
-	searchCommand(command[0],path);
+	searchCommand(command,path);
+	executeCommand(command,path);
 	return 0;
 
 }
